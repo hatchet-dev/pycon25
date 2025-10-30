906b13c3060194242028384453ef4c17976f8d11
mrkaye97 <mrkaye97@gmail.com>
2025-10-30 16:33:48 +0100

    fix: dep signature

--- src/common/dependencies.py
+++ src/common/dependencies.py
@@ -1,10 +1,10 @@
-from typing import Annotated
+from typing import Annotated, Any
 
 from hatchet_sdk import Depends
 from openai import AsyncOpenAI
 
 
-async def openai_client() -> AsyncOpenAI:
+async def openai_client(_i: Any, _c: Any) -> AsyncOpenAI:
     return AsyncOpenAI()
 
 
